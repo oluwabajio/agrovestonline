dc2185a4238dda9d0cc5ea8ba4ac41fe7597c435
Drivers-Ng <35292765+Drivers-Ng@users.noreply.github.com>
2018-05-30 12:20:30 +0100

    agrovest commit 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+ <!-- Page Content -->
+ <div class="container">
+
+<!-- Heading Row -->
+<div class="row my-4">
+  <div class="col-lg-8">
+    <img class="img-fluid rounded" src="http://placehold.it/900x400" alt="">
+  </div>
+  <!-- /.col-lg-8 -->
+  <div class="col-lg-4">
+    <h1>Agricultural Advice at your finger tips</h1>
+    <p>Get quality answers to your agricultural questions in less than 10 minutes.</p>
+    <a class="btn btn-primary btn-lg" href="#">Ask Questions Now</a>
+  </div>
+  <!-- /.col-md-4 -->
+</div>
+<!-- /.row -->
+
+
+<!-- Content Row -->
+<div class="row">
+
+<div class="col-md-12">
+<h3 class="text-center" style="margin-top:40px; color:#000; font-weight: 6000;">Welcome To Agrovest Online Consultancy Service</h3>
+</div>
+
+<div class="col-sm-2">
+
+</div>
+<div class="col-sm-8">
+
+<p class="text-center lead" data-aos="zoom-up" data-aos-once="true" style="margin-bottom:38px">
+    With over 37 Million businesses in Nigeria, this poll is aimed at recognizing the top 100 emerging SMEs as nominated by our audience
+</p>
+</div>
+
+</div>
+
+
+
+<div class="row">
+  <div class="col-md-4 mb-4">
+    <div class="card h-100">
+      <div class="card-body">
+      <img class="img-fluid rounded" src="http://placehold.it/400x200" alt="">
+        <h3 class="card-title" style="margin-top:18px">Card One</h3>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Ask Us a Question</a>
+      </div>
+    </div>
+  </div>
+  <!-- /.col-md-4 -->
+  <div class="col-md-4 mb-4">
+    <div class="card h-100">
+      <div class="card-body">
+      <img class="img-fluid rounded" src="http://placehold.it/400x200" alt="">
+        <h3 class="card-title" style="margin-top:18px">Card Two</h3>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Ask Us a Question</a>
+      </div>
+    </div>
+  </div>
+  <!-- /.col-md-4 -->
+  <div class="col-md-4 mb-4">
+    <div class="card h-100">
+      <div class="card-body">
+      <img class="img-fluid rounded" src="http://placehold.it/400x200" alt="">
+        <h3 class="card-title" style="margin-top:18px">Card Three</h3>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary">Ask Us a Question</a>
+      </div>
+    </div>
+  </div>
+  <!-- /.col-md-4 -->
+
+</div>
+<!-- /.row -->
+
+
+
+
+<section class="p0 mb64 mb-xs-32">
+    <div class="container-fluid">
+        <div class="row mb24 mb-xs-16">
+            <div class="col-md-12">
+                <hr>
+                <h3 class="text-center c-black font600 mt64 mt-xs-32">Top 8 SME Nominees</h3>
+                <p class="text-center lead">
+                   Here are the top voted businesses as of May 29, 2018               
+                </p>
+            </div>
+        </div>
+        
+       </div>
+</section>
+
+ <!-- footer section start -->
+
+@endsection
